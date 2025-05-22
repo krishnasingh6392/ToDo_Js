9c5666ad2da44d9b255e720a5a5534c6f7c0e4c5
Krishna Singh <kn168679@gmail.com>
2025-05-22 13:16:53 +0530

    updated checkbox style and functionality

--- todo.js
+++ todo.js
@@ -3,40 +3,28 @@ let items = document.getElementById("todo-items");
 let todolist = document.getElementById("todo-list");
 button.addEventListener("click", () => {
   let todoitems = document.createElement("li");
-  todoitems.innerHTML = `  <input type="checkbox" class="check-box text-red-400 " />
+  todoitems.innerHTML = `
+    <span class="flex justify-center items-center gap-1">
+      <input type="checkbox" class="check-box accent-green-500  w-4 h-4  rounded-full border-2 border-green-500 " />
   <p class="task-text">${items.value}</p>
+    </span>
 
-         <button
-         
-            class="bg-blue-300 px-4  rounded-md ml-2 border border-none edit-btn "
-          >
-            Edit
-        </button>
+      
 
          <button
             
-            class= " bg-red-400 px-4  rounded-md ml-2 border border-none delete-btn "
+            class= "  ml-2 border border-none delete-btn "
           >
-            Del
+       <i class="fa-solid fa-trash text-red-500 "></i>
         </button>
 
   `;
   todoitems.className =
-    "my-4  w-full  bg-slate-300  h-9 flex justify-center items-center   rounded-md  gap-8  rounded-r-full rounded-l-full ";
+    " flex justify-between m-4 px-4 items-center  py-2 mx-8 rounded-l-full rounded-r-full bg-slate-100 placeholder:text-slate-600 text-center";
   let checkbox = todoitems.querySelector(".check-box");
   let taskText = todoitems.querySelector(".task-text");
   let dltbtn = todoitems.querySelector(".delete-btn");
   let editbtn = todoitems.querySelector(".edit-btn");
-
-  //   editbtn.addEventListener("click", () => {
-  //     let input = document.createElement("input");
-  //     input.value = taskText.textContent;
-  //     input.className =
-  //       "my-4   bg-slate-300  h-9 flex justify-center items-center   rounded-md  gap-8 mx-8 rounded-r-full rounded-l-full ";
-  //     todoitems.replaceChild(input, taskText);
-  //     editbtn.textContent = "Save";
-  //     taskText = input;
-  //   });
 
   dltbtn.addEventListener("click", () => {
     todoitems.remove();
@@ -44,7 +32,7 @@ button.addEventListener("click", () => {
 
   checkbox.addEventListener("change", () => {
     taskText.classList.toggle("line-through");
-    taskText.classList.toggle("text-red-400");
+    taskText.classList.toggle("text-green-700");
   });
 
   todolist.appendChild(todoitems);
